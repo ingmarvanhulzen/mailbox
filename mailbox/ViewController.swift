1223e24cd0159aebae24afa00dac4f1ff5048531
ivanhulzen <ivanhulzen@jouwomgeving.nl>
2020-08-24 14:06:24 +0200

    Initial Commit

---
+++ mailbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mailbox
+//
+//  Created by Ingmar van Hulzen on 24/08/2020.
+//  Copyright © 2020 Ingmar van Hulzen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
